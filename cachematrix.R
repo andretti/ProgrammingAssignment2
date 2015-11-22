662b3c38a3c92bd15625b15ed447ed5ee3e1f464
Andretti <7b992422@opayq.com>
2015-11-22 16:13:09 -0500

    Added second function to cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -7,6 +7,7 @@
 # to the function as argument.
 # The function will invert the matrix, get or set the matrix, 
 # or invert the given matrix.
+#
 makeCacheMatrix <- function(x = matrix()){
     
     # initialize matrix object
@@ -23,4 +24,24 @@ makeCacheMatrix <- function(x = matrix()){
     list(set = set, get = get, 
          setmatrix = setmatrix,
          getmatrix = getmatrix)
+}
+
+
+# The function first checks if the inverted matrix has 
+# already been created.  If so, it retrieves it from 
+# the cache else the function will create it and 
+# will save the new matrix to the cache
+# 
+cacheSolve <- function(x, ...){
+    m <- x$getmatrix()
+    
+    if(!is.null(m)){
+        message("getting the cached matrix data")
+        return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setmatrix(m)
+    m
 }

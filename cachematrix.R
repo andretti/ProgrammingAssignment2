f73f294f69f3eb1be89b2aa8571a8dc9db3f0b38
Andretti <7b992422@opayq.com>
2015-11-22 16:02:42 -0500

    Updated file cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Coursera
+# R-Programming
+# Assignment 2
+# Creates the inverted matrix
+# 
+# This function will create a special "inverted matrix" passed 
+# to the function as argument.
+# The function will invert the matrix, get or set the matrix, 
+# or invert the given matrix.
+makeCacheMatrix <- function(x = matrix()){
+    
+    # initialize matrix object
+    m <- NULL
+    
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setmatrix <- function(solve) m <<- solve
+    getmatrix <- function() m
+    list(set = set, get = get, 
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
